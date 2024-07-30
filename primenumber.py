2eb5dd11d3c64a38f3c7693959a90a82f11a8bb1
Serhet Gökdemir <apocalptica38@gmail.com>
2024-07-30 23:59:37 +0300

    prime number checking

---
+++ primenumber.py
@@ -0,0 +1,14 @@
+def primeCheck(number):
+    isPrime = True
+    for i in range(2,number):
+        if (number%i == 0):
+            isPrime = False
+            break
+    return isPrime
+
+
+if __name__ == "__main__":
+    while True:
+        number = int(input("Enter the # : "))
+        print(primeCheck(number))
+        if number == 0: break
