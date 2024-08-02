c425c28f67bf2ded06ba04ac9600d1a98ad6a67c
Serhet Gökdemir <apocalptica38@gmail.com>
2024-08-02 13:27:37 +0300

    özel durumlar eklendi ve düzeltmeler yapıldı

--- calculator.py
+++ calculator.py
@@ -6,14 +6,23 @@ def calculator():
     while True:
         opr = input("Operatörü giriniz: ")
 
+        if opr == "Q":
+            print("Hesap makinesi programından çıkılıyor...")
+            break
         if opr == "C": Sayi = 0
         else: 
-            Sayi2 = int(input("Sayı:"))
-            if opr == "+": Sayi+=Sayi2
-            elif opr == "-": Sayi-=Sayi2
-            elif opr == "*": Sayi*=Sayi2
-            elif opr == "/": Sayi/=Sayi2
-            else: print("Geçersiz operatör girdiniz.")
+            try:
+                Sayi2 = int(input("Sayı:"))
+                if opr == "+": Sayi+=Sayi2
+                elif opr == "-": Sayi-=Sayi2
+                elif opr == "*": Sayi*=Sayi2
+                elif opr == "/": 
+                    try: Sayi/=Sayi2
+                    except ZeroDivisionError:
+                        print("Sayı 0'a bölünemez.")    
+                else: print("Geçersiz operatör girdiniz.")
+            except ValueError:
+                print("Geçerli bir sayı giriniz.")
 
         print(Sayi)
 
@@ -22,4 +31,5 @@ if __name__ == "__main__":
     print("---------Basit hesap makinesi programı---------")
     print("Geçerli operatörler: '+'  '-'  '*'  '/'")
     print("Sıfırlamak için 'C' yazınız.")
+    print("Çıkış Yapmak için 'Q' yazınız.")
     calculator()
